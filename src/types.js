b7107ac850e7529d45a8b6d26db0fef1b3e2284c
brandon <gwonzhang@gmail.com>
2023-05-26 23:03:05 +0800

    add types.js

---
+++ src/types.js
@@ -0,0 +1,22 @@
+/**
+ * @typedef {Object} Route
+ * @property {string} [path] - The path of the route
+ * @property {Function} [handler] - The handler function for the route.
+ */
+
+/**
+ * @typedef {Object.<string, string>} RouteParams
+ */
+
+/**
+ * @typedef {Object} MatchedRoute
+ * @property {Route} route
+ * @property {RouteParams} params
+ */
+
+export {
+  Route,
+  RouteParams,
+  MatchedRoute,
+}
+
