dc42da8e20d523ae9c10444cc3387c892007a29f
brandon <gwonzhang@gmail.com>
2023-05-27 13:59:19 +0800

    update package name inside test/

--- test/index.js
+++ test/index.js
@@ -1,13 +1,18 @@
-import BreezeRouter from "@heybran/breeze-spa-router";
+// @ts-check
+import BreezeRouter from "breeze-spa-router";
 
 const output = document.createElement('output');
 document.body.append(output);
 
 // Initialize the router
-window.ROUTER = new BreezeRouter(document.querySelector('#app'));
+const ROUTER = new BreezeRouter();
 
-ROUTER.add('/', async () => {
-  output.textContent = 'Home';
+ROUTER.add('/', async ({ route, params }) => {
+  output.textContent = `
+    ${JSON.stringify({ route} , null, 2)}
+    ${JSON.stringify({ params }, null, 2)}
+  `;
+  console.log({ route, params });
 });
 
 ROUTER.add('/users/:id', async ({ route, params }) => {
@@ -27,3 +32,4 @@ ROUTER.add('/users/:username/posts/:postId', async ({ route, params }) => {
 });
 
 ROUTER.init();
+window.ROUTER = ROUTER;
