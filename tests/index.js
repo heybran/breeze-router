693083dccede9b711c9dfe807a127dcaca43c3fd
brandon <gwonzhang@gmail.com>
2023-06-07 11:56:27 +0800

    chore: update tests files

---
+++ tests/index.js
@@ -0,0 +1,61 @@
+// @ts-check
+import BreezeRouter from "breeze-spa-router";
+
+const siteTitle = "A Fabulous Company";
+
+const renderPage = async (path) => {
+  const html = `./pages/${path}.html`;
+  const response = await fetch(html);
+  const data = await response.text();
+  document.querySelector("#root").innerHTML = data;
+  document.title = "About Us";
+};
+
+// Initialize the router
+const ROUTER = new BreezeRouter();
+
+ROUTER.add("/", async ({ route, params }) => {
+  console.log({ route, params });
+  await renderPage("/home");
+  console.log("home page rendered");
+});
+
+ROUTER.add("/brandon", ({ route, params }) => {
+  console.log({ route, params });
+  ROUTER.redirect("/about");
+});
+
+ROUTER.add("/about", async ({ route, params }) => {
+  console.log({ route, params });
+  await renderPage(route.path);
+  console.log(`${route.path} page rendered`);
+});
+
+ROUTER.add("/services", async ({ route, params }) => {
+  console.log({ route, params });
+  await renderPage(route.path);
+  console.log(`${route.path} page rendered`);
+});
+
+ROUTER.add("/contact", async ({ route, params }) => {
+  console.log({ route, params });
+  await renderPage(route.path);
+  console.log(`${route.path} page rendered`);
+});
+
+ROUTER.add("/users/:id", async ({ route, params }) => {
+  console.log({ route, params });
+});
+
+ROUTER.add("/users/:username/posts/:postId", async ({ route, params }) => {
+  console.log({ route, params });
+});
+
+ROUTER.add("404", async ({ route, params }) => {
+  console.log({ route, params });
+  await renderPage(route.path);
+  console.log(`${route.path} page rendered`);
+});
+
+ROUTER.init();
+window.ROUTER = ROUTER;
